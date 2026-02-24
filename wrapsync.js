089fd2990c758fbd4e728e69a0fa6d071c80c080
YASHARTH1630 <yasharth0825@kgpian.iitkgp.ac.in>
2026-02-24 20:36:59 +0530

    Update wrapsync.js

--- wrapsync.js
+++ wrapsync.js
@@ -2,4 +2,4 @@ module.exports = function asyncWrap(fn) {
     return function(req, res, next) {
         fn(req, res, next).catch((err) => { next(err); })
     }
-}; //try & catch ka substitute , async ke problem ko handle k liye
+}; 
